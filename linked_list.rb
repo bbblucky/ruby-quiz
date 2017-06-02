a344d51b5ceb942b5af6fef995b1d888a5892893
Yi Liu <bbblucky@hotmail.com>
2017-06-02 16:25:42 -0400

    Add linked list in ruby

---
+++ linked_list.rb
@@ -0,0 +1,44 @@
+class Node
+  attr_accessor :node, :next
+
+  def self.last(node)
+    return node if node.next.nil?
+    node = last(node.next)
+  end
+
+  def self.reverse(node)
+    return node if node.next.nil?
+    head, swap, node.next = node.next, node, nil
+    link = head.next
+    while link != nil
+      head.next = swap
+      swap = head
+      head = link
+      link = link.next
+    end
+    head.next = swap
+    head
+  end
+
+  def self.node_list(node, msg = nil)
+    msg ||= ""
+    return msg[0..-4] if node.nil?
+    node_list(node.next, msg << "#{node.node} -> ")
+  end
+
+
+  def initialize(node)
+    @node = node
+  end
+end
+
+node = Node.new(1)
+100.times do |i|
+  i += 1
+  eval("Node.last(node).next = Node.new(i + 1)")
+end
+
+puts Node.node_list node
+puts "========="
+puts Node.node_list Node.reverse(node)
+puts Node.last(node).node
